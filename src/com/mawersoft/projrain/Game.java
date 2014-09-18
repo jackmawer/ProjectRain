bac4fc28fadb595b16237e8aa91a39426c5c054f
Jack Mawer <jack@mawersoft.co.uk>
2014-09-18 16:14:50 +0100

    0.0.6 Release

--- src/com/mawersoft/projrain/Game.java
+++ src/com/mawersoft/projrain/Game.java
@@ -28,7 +28,7 @@ public class Game extends Canvas implements Runnable {
 	private static int width = 300;
 	private static int height = width / 16 * 9;
 	private static int scale = 3;
-	public static String ver = "0.0.5";
+	public static String ver = "0.0.6";
 	//public static String title = "ProjRain";
 	public static String title = "Project Rain "+ver;
 	public int currentFPS;
